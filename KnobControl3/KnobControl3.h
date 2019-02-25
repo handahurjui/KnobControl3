147eb3092b42ef761fa672b5422a87aa62a470ed
Andreea Hurjui <Andreea.Hurjui@endava.com>
2019-02-25 16:23:18 +0200

    Initial commit

---
+++ KnobControl3/KnobControl3.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl3.h
+//  KnobControl3
+//
+//  Created by Andreea Hurjui on 25/02/2019.
+//  Copyright © 2019 Andreea Hurjui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KnobControl3.
+FOUNDATION_EXPORT double KnobControl3VersionNumber;
+
+//! Project version string for KnobControl3.
+FOUNDATION_EXPORT const unsigned char KnobControl3VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl3/PublicHeader.h>
+
+
